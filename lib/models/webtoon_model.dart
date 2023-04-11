141835cdd062c1d0f826df707c6349bec23f8a5d
jakejo <cakim21@naver.com>
2023-04-11 23:27:24 +0900

    webtoon flex

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -5,7 +5,6 @@ class WebtoonModel {
       : title = json['title'],
         thumb = json['thumb'],
         id = json['id'];
-
   // WebtoonModel(Map<String, dynamic> json) {
   //   title = json['title'];
   //   thumb = json['thumb'];
